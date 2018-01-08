33be7f56fa885e734313310c8349f96c6deead3a
Marcus Baw <marcusbaw@gmail.com>
2018-01-08 11:28:18 +0000

    updates to Opal 0.10.0 and misc changes to details in setup()

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 long_desc = """
-OPAL Lab is a plugin for the OPAL web framework. It allows an extensible way of
+Opal Lab is a plugin for the OPAL web framework. It allows an extensible way of
 modelling lab tests.
 
 Source code and documentation available at https://github.com/openhealthcare/opal-lab/
@@ -13,17 +13,17 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='opal-lab',
-    version='0.2',
+    version='0.4.0',
     packages=find_packages(),
     include_package_data=True,
     license='LICENSE',
-    description='The lab OPAL Plugin',
+    description='The Opal lab Plugin',
     long_description=long_desc,
     url='http://opal.openhealthcare.org.uk/',
     author='Open Health Care UK',
     author_email='hello@openhealthcare.org.uk',
     install_requires=[
-        'opal>=0.8.1',
+        'opal>=0.10.0',
         'django-choices==1.4.4',
         'jsonfield==1.0.3'
     ]
