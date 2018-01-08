8ac7a78fd3878eac806e37ef5639c8d4c57d85db
Marcus Baw <marcusbaw@gmail.com>
2018-01-08 13:48:14 +0000

    added Django >1.8 TEMPLATE = [] list in setup

--- runtests.py
+++ runtests.py
@@ -52,6 +52,28 @@ settings.configure(DEBUG=True,
                        'lab': 'lab.nomigrations',
                        'lab.tests': 'lab.tests.nomigrations'
                    },
+                   TEMPLATES = [
+                    {
+                        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                        'DIRS': [
+                            # insert your TEMPLATE_DIRS here
+                        ],
+                        'APP_DIRS': True,
+                        'OPTIONS': {
+                            'context_processors': [
+                                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                                # list if you haven't customized them:
+                                'django.contrib.auth.context_processors.auth',
+                                'django.template.context_processors.debug',
+                                'django.template.context_processors.i18n',
+                                'django.template.context_processors.media',
+                                'django.template.context_processors.static',
+                                'django.template.context_processors.tz',
+                                'django.contrib.messages.context_processors.messages',
+                            ],
+                        },
+                    },
+                ],
 )
 
 import django
