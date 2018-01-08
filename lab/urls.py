18a943bc716c5dac04e5d4bda0a74c5d8afeb6e3
Marcus Baw <marcusbaw@gmail.com>
2018-01-08 11:16:21 +0000

    url refactoring for Django 10

--- lab/urls.py
+++ lab/urls.py
@@ -1,14 +1,14 @@
 """
-Urls for the lab OPAL plugin
+Urls for the lab Opal plugin
 """
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from lab import views
 
-urlpatterns = patterns(
+urlpatterns = [
     '',
     url(r'^templates/lab_tests/forms/(?P<model>[0-9a-z_\-]+)_form.html/?$',
         views.LabTestResultTemplateView.as_view(), name="lab_form_view"),
     url(r'^templates/lab_tests/record/(?P<model>[0-9a-z_\-]+).html/?$',
         views.LabTestRecordTemplateView.as_view(), name="lab_record_view"),
-)
+]
